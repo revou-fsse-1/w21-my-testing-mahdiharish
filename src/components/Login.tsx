7d96a3454ff76c723bee97c1cafe9b85f9a433ab
mahdiharish <mahdiharish12@gmail.com>
2023-07-07 00:53:12 +0800

    testing for login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -127,18 +127,18 @@ const handleLogin = async (data: LoginFormValues) => {
 
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div data-testid="login" className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-lg">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
         <form onSubmit={handleSubmit(handleLogin)}>
           <div className="mb-4">
-            <label className="block mb-2">Email:</label>
-            <input type="text" {...register('email')} className="border border-gray-300 px-3 py-2 rounded w-full" />
+            <label htmlFor="email" className="block mb-2">Email:</label>
+            <input id="email" type="text" {...register('email')} className="border border-gray-300 px-3 py-2 rounded w-full" />
             {errors.email && <p>{errors.email.message}</p>}
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Password:</label>
-            <input type="password" {...register('password')} className="border border-gray-300 px-3 py-2 rounded w-full" />
+            <label htmlFor="password" className="block mb-2">Password:</label>
+            <input id="password" type="password" {...register('password')} className="border border-gray-300 px-3 py-2 rounded w-full" />
             {errors.password && <p>{errors.password.message}</p>}
           </div>
           <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>

b3da919b6a1916b97476361f93f8d49e030a332f
mahdiharish <mahdiharish12@gmail.com>
2023-07-06 22:26:04 +0800

    testing for register done

---
+++ src/components/Register.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+import Register from "./Register"
+import { BrowserRouter as Router } from "react-router-dom"
+import * as yup from 'yup';
+
+test('render Register page correctly', () => {
+    render(
+        <Router>
+            <Register />
+        </Router>
+    )
+    const registerPage = screen.getByTestId('register');
+    expect(registerPage).toBeTruthy()
+})
+
+describe('Register Form Validation Schema', () => {
+  test('should require name', async () => {
+    const schema = yup.object().shape({
+      name: yup.string().required('Name is required'),
+    });
+    const validData = { name: 'John Doe' };
+    const invalidData = { name: '' };
+    await expect(schema.validate(validData)).resolves.toBe(validData);
+    await expect(schema.validate(invalidData)).rejects.toThrow('Name is required');
+  });
+
+  test('should require a valid email', async () => {
+    const schema = yup.object().shape({
+      email: yup.string().required('Email is required').email('Invalid email address'),
+    });
+    const validData = { email: 'test@example.com' };
+    const invalidData = { email: 'invalid_email' };
+    await expect(schema.validate(validData)).resolves.toBe(validData);
+    await expect(schema.validate(invalidData)).rejects.toThrow('Invalid email address');
+  });
+
+  test('should require a password of at least 8 characters', async () => {
+    const schema = yup.object().shape({
+      password: yup.string().required('Password is required').min(8, 'Password must be at least 8 characters long'),
+    });
+    const validData = { password: 'password123' };
+    const invalidData = { password: 'short' };
+    await expect(schema.validate(validData)).resolves.toBe(validData);
+    await expect(schema.validate(invalidData)).rejects.toThrow('Password must be at least 8 characters long');
+  });
+});
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -40,21 +40,21 @@ function Register() {
   return (
     <div data-testid="register" className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-lg">
-        <h2 className="text-2xl font-bold mb-4">Register</h2>
+        <h2 className="text-2xl font-bold mb-4">Registration Page</h2>
         <form onSubmit={handleSubmit(handleRegister)}>
             <div className="mb-4">
-              <label className="block mb-2">Name:</label>
-              <input type="text" {...register('name')} className="border border-gray-300 px-3 py-2 rounded w-full" />
+              <label htmlFor="name" className="block mb-2">Name:</label>
+              <input id="name" type="text" {...register('name')} className="border border-gray-300 px-3 py-2 rounded w-full" />
               {errors.name && <p>{errors.name.message}</p>}
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Email:</label>
-            <input type="text" {...register('email')} className="border border-gray-300 px-3 py-2 rounded w-full" />
+            <label htmlFor="email" className="block mb-2">Email:</label>
+            <input id="email" type="text" {...register('email')} className="border border-gray-300 px-3 py-2 rounded w-full" />
             {errors.email && <p>{errors.email.message}</p>}
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Password:</label>
-            <input type="password" {...register('password')} className="border border-gray-300 px-3 py-2 rounded w-full" />
+            <label htmlFor="password" className="block mb-2">Password:</label>
+            <input id="password" type="password" {...register('password')} className="border border-gray-300 px-3 py-2 rounded w-full" />
             {errors.password && <p>{errors.password.message}</p>}
           </div>
           <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Register</button>
